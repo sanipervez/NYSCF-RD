fcb57252cca10695013c53db343504ffb0bdbba2
Sania Pervez <sanipervez@users.noreply.github.com>
2025-07-11 14:10:37 -0400

    passcode logic implemented

--- ui/landing_page.c
+++ ui/landing_page.c
@@ -5,10 +5,6 @@ GtkWidget* create_landing_page() {
     // Create main vertical box
     GtkWidget *main_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
 
-    // Title label
-    GtkWidget *welcome = gtk_label_new("Setpoint");
-    gtk_widget_set_name(welcome, "landing-title");
-
     // Create grid for side-by-side buttons
     GtkWidget *grid = gtk_grid_new();
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
@@ -34,7 +30,6 @@ GtkWidget* create_landing_page() {
     gtk_grid_attach(GTK_GRID(grid), oxygen_button, 1, 0, 1, 1);
 
     // Pack into main_vbox
-    gtk_box_pack_start(GTK_BOX(main_vbox), welcome, FALSE, FALSE, 10);
     gtk_box_pack_start(GTK_BOX(main_vbox), grid, TRUE, TRUE, 0);
 
     return main_vbox;
--- ui/passcode_page.c
+++ ui/passcode_page.c
@@ -9,12 +9,23 @@ char entered_passcode[PASSCODE_LENGTH + 1] = "";
 GtkWidget *entry;
 
 void check_passcode() {
-    if (strcmp(entered_passcode, CORRECT_PASSCODE) == 0) {
-        gtk_entry_set_text(GTK_ENTRY(entry), "Access Granted");
-    } else {
-        gtk_entry_set_text(GTK_ENTRY(entry), "Access Denied");
-    }
+    GtkWidget *dialog;
+
+    if (strcmp(entered_passcode, CORRECT_PASSCODE) != 0) {
+        dialog = gtk_message_dialog_new(
+            GTK_WINDOW(gtk_widget_get_toplevel(entry)),
+            GTK_DIALOG_MODAL,
+            GTK_MESSAGE_INFO,
+            GTK_BUTTONS_OK,
+            "Access Denied"
+        );
+    } 
+
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+
     memset(entered_passcode, 0, sizeof(entered_passcode));
+    gtk_entry_set_text(GTK_ENTRY(entry), "");
 }
 
 void on_digit_clicked(GtkButton *button, gpointer user_data) {
@@ -23,10 +34,6 @@ void on_digit_clicked(GtkButton *button, gpointer user_data) {
     if (strlen(entered_passcode) < PASSCODE_LENGTH) {
         strcat(entered_passcode, digit);
         gtk_entry_set_text(GTK_ENTRY(entry), entered_passcode);
-    }
-
-    if (strlen(entered_passcode) == PASSCODE_LENGTH) {
-        check_passcode();
     }
 }
 
@@ -62,6 +69,8 @@ GtkWidget* create_passcode_keypad() {
     gtk_widget_set_hexpand(entry, TRUE);
     gtk_widget_set_vexpand(entry, TRUE);
     gtk_grid_attach(GTK_GRID(grid), entry, 0, 0, 3, 1);
+    gtk_entry_set_alignment(GTK_ENTRY(entry), 0.5);
+    gtk_widget_set_name(entry, "digits");
 
     int number = 1;
     for (int i = 0; i < 3; i++) {
@@ -73,8 +82,6 @@ GtkWidget* create_passcode_keypad() {
             gtk_widget_set_vexpand(button, TRUE);
             g_signal_connect(button, "clicked", G_CALLBACK(on_digit_clicked), NULL);
             gtk_grid_attach(GTK_GRID(grid), button, j, i + 1, 1, 1);
-            g_print("Button name: %s\n", gtk_widget_get_name(button));
-
             number++;
         }
     }
@@ -86,7 +93,6 @@ GtkWidget* create_passcode_keypad() {
     g_signal_connect(back_button, "clicked", G_CALLBACK(on_backspace_clicked), NULL);
     gtk_grid_attach(GTK_GRID(grid), back_button, 0, 4, 1, 1);
     gtk_widget_set_name(back_button, "passcode-button");
-
 
     // Zero button
     GtkWidget *zero_button = gtk_button_new_with_label("0");
@@ -104,26 +110,19 @@ GtkWidget* create_passcode_keypad() {
     gtk_grid_attach(GTK_GRID(grid), enter_button, 2, 4, 1, 1);
     gtk_widget_set_name(enter_button, "passcode-button");
 
-
     return grid;
 }
-
 
 GtkWidget* create_passcode_page() {
     GtkWidget *outer_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_widget_set_hexpand(outer_box, TRUE);
     gtk_widget_set_vexpand(outer_box, TRUE);
-    
-    // Title
-    GtkWidget *welcome = gtk_label_new("Enter Passcode");
-    gtk_widget_set_name(welcome, "passcode-title");
+
 
     // Keypad
     GtkWidget *keypad = create_passcode_keypad();
 
     // Pack everything
-
-    gtk_box_pack_start(GTK_BOX(outer_box), welcome, FALSE, FALSE, 5);
     gtk_box_pack_start(GTK_BOX(outer_box), keypad, TRUE, TRUE, 10);
 
     return outer_box;
